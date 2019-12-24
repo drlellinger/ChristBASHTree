1bbe4ad74e101661f3627bb42171eccec2b5a4eb
drlellinger <39723641+drlellinger@users.noreply.github.com>
2019-12-24 17:45:39 +0100

    Update tree-DE.sh

--- tree-DE.sh
+++ tree-DE.sh
@@ -32,8 +32,7 @@ for ((i=1; i<=2; i++))
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo FROHE WEIHNACHTEN
-tput cup $((lin + 1)) $((c - 10)); echo Und ein frohes neues Jahr $new_year
+tput cup $lin $((c - 6)); echo FROHE WEIHNACHTEN /n echo Und ein frohes neues Jahr $new_year
 let c++
 k=1
 
